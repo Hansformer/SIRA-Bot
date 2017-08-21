0ebadda02fc761fc208d1d92d7740a19591f8140
N2O-Powered <allan.dobro91@gmail.com>
2017-08-20 21:56:26 -0400

    dank

--- init.py
+++ init.py
@@ -13,29 +13,46 @@ from config import *
 client = discord.Client()
 chan = None
 
-
 @client.event
 @asyncio.coroutine
 def on_ready():
-    print(client.user.name)
-    print(client.user.id)
-    print('--------------')
-    print('I live')
+	print(client.user.name)
+	print(client.user.id)
+	print('--------------')
+	print('I live')
 
 
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    if message.channel.name == 'sira-bot-playground':
-        chan = message.channel
-        if message.content.startswith('*bombs u*'):
-            yield from client.send_message(chan,
-                                           'Space IRA will be free!')
-        if message.content.startswith('<@319826689729232897>'):
-            yield from client.send_message(chan,
-                                           'You mentioned me')
-    if debug:
-        print("New message in: %s: %s" % (message.channel.name, message.content))
+	chan = message.channel
+	
+	if message.channel.name != 'fleetcom' or 'human-resources' or 'ayy-lmao-info-hub':
+		if message.content.startswith('*bombs u*'):
+			yield from client.send_message(chan,'Space Ireland will be free! <:space_ireland:309204831548211201>')
+
+		elif message.content.startswith('<@!319826689729232897>'):
+			yield from client.send_message(chan,'You noticed me, senpai. <:anime_smug:319973746825756683>')
+
+	if message.content.startswith('!botkill'):
+		if message.author.id == '189890760873738240':
+			chan = client.get_channel('348971376750886912')
+			yield from client.send_message(chan,'sira-bot signing off o777')
+			yield from client.close()
+
+	if message.content.find('<:space_ireland:309204831548211201>') != -1:
+		yield from client.add_reaction(message, ':space_ireland:309204831548211201')
+		
+	x = message.content.lower()
+	if x.find('space ireland') != -1:
+		yield from client.add_reaction(message, ':space_ireland:309204831548211201')
+	if x.find('s p a c e i r e l a n d') != -1:
+		yield from client.add_reaction(message, ':space_ireland:309204831548211201')
+
+	if debug:
+		print("New message (%s) - %s: %s" % (message.channel, message.author, message.content))
 
 client.login(username, password)
 client.run(token)
+
+
