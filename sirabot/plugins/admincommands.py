d0de4c7ecaff28e0fe4b0d1daa9633fdc7875538
N2O-Powered <mail@adobro.online>
2017-08-22 14:33:00 -0400

    !vision was broke, added !status1 and !status2 as debugs

--- sirabot/plugins/admincommands.py
+++ sirabot/plugins/admincommands.py
@@ -15,19 +15,22 @@ async def kill(client, message, parameter):
 @is_admin
 async def idle(client, message, parameter):
     await client.change_presence(game=discord.Game(name='recharging'),
-                                 status=discord.Status('idle'), afk=True)
+                                 status=discord.Status('idle'), 
+                                 afk=True)
 
 
 @is_admin
 async def vision(client, message, parameter):
     await client.change_presence(game=discord.Game(name='v i s i o n'),
-                                 status=discord.Status('online'), afk=False)
+                                 status=discord.Status('online'), 
+                                 afk=False)
 
 
 async def setup(client):
     for alias in ['botkill', 'kill', 'close', 'end',
                   'ded', 'rip', 'makeded', 'fuckoff']:
         client.register_command(alias, kill)
-    for alias in ['rest', 'idle', 'recharge']:
+    for alias in ['rest', 'idle', 'recharge','status2']:
         client.register_command(alias, idle)
-    client.register_command('vision', idle)
+    for alias in ['vision','status1']:
+        client.register_command(alias, vision)
