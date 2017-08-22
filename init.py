25cc49e3151c281b6877c5603b6170c5ed2bbfeb
N2O-Powered <allan.dobro91@gmail.com>
2017-08-21 22:43:10 -0400

    refactored remaining ! commands and more pep8

--- init.py
+++ init.py
@@ -1,10 +1,10 @@
 # required libs
-import discord
-import asyncio
-import re
-import pendulum
 import aiohttp
+import asyncio
+import discord
 import json
+import pendulum
+import re
 
 # discord api config
 '''
@@ -21,21 +21,27 @@ from config import *
 # var defs
 client = discord.Client()
 chan = None
-sstatus = None
+# server status
 smsg = None
+sstatus = None
+# timestamp
 tstamp = None
 
 
+# timestamp formatting for console/terminal
 def get_time():
     zone = pendulum.timezone(tzone)
     stamp = pendulum.now(zone).strftime(tformat)
     return stamp
 
 
+# get server status from EDSM API
 @client.event
 async def check_server():
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.edsm.net/api-status-v1/elite-server') as resp:
+        async with session.get(
+            'https://www.edsm.net/api-status-v1/elite-server'
+                ) as resp:
             api = await resp.json()
             return api['type'], api['message']
 
@@ -66,11 +72,11 @@ def on_member_join(member):
     tstamp = get_time()
     print("[%s] User joined - %s" % (tstamp, member.name))
     chan = client.get_channel('195647497505472512')
-    m = member.id
     yield from client.send_message(
         chan,
         "Welcome <@!%s>. <:vision_intensifies:332951986645499904> "
-        "If you have any issues, please tag an <@&200367057378869248>." % m)
+        "If you have any issues, please tag an <@&200367057378869248>."
+        % member.id)
     yield from client.send_message(
         chan,
         "Join the /edg/ player group, SIRA - https://inara.cz/wing/1470")
@@ -103,9 +109,9 @@ def on_message(message):
         if(message.author.id == '189890760873738240' or
            message.author.id == '156405315976429568'
            ):
-            
+
             if command in ['botkill', 'kill', 'close', 'end', 'ded', 'rip',
-                'makeded', 'fuckoff']:
+                           'makeded', 'fuckoff']:
 
                 # send a message and kill the script
                 print("[%s] SIRA Bot disengaged." % tstamp)
@@ -118,23 +124,53 @@ def on_message(message):
             # idle
             if command in ['rest', 'idle', 'recharge']:
                 yield from client.change_presence(
-                    game=discord.Game(name='recharging'),
-                    status=discord.Status('idle'),
+                    game=discord.Game(
+                        name='recharging'),
+                    status=discord.Status(
+                        'idle'),
                     afk=True)
 
             # vision
             if command in ['vision']:
                 yield from client.change_presence(
-                    game=discord.Game(name='v i s i o n'),
-                    status=discord.Status('online'),
+                    game=discord.Game(
+                        name='v i s i o n'),
+                    status=discord.Status(
+                        'online'),
                     afk=False)
+
+        # server status
+        if command in ['server', 'status']:
+            sstatus, smsg = yield from check_server()
+            if sstatus == 'success':
+                yield from client.send_message(
+                    chan,
+                    'FDev says "%s". :ok_hand:'
+                    % smsg)
+            elif sstatus == 'warning':
+                yield from client.send_message(
+                    chan,
+                    ':warning: FDev says "%s".'
+                    % smsg)
+            elif sstatus == 'danger':
+                yield from client.send_message(
+                    chan,
+                    ':fire: "%s". Sandro tripped over the server cords again.'
+                    % smsg)
+
+        # flag
+        if command in ['flag']:
+            yield from client.send_file(
+                chan,
+                "C:/SIRA/SIRA-Bot-Rebirth/flag_of_space_ireland.png")
 
     # no self reactions
     if message.author.id != '319826689729232897':
         # space ireland reactions
         if(message.content.find('<:space_ireland:309204831548211201>') != -1 or
-           re.search(r'(s\s?p\s?a\s?c\s?e\s*i\s?r\s?e\s?l\s?a\s?n\s?d)+\b',
-                     x)
+           re.search(
+                r'(s\s?p\s?a\s?c\s?e\s*i\s?r\s?e\s?l\s?a\s?n\s?d)+\b',
+                x)
            ):
             yield from client.add_reaction(
                 message,
@@ -144,14 +180,17 @@ def on_message(message):
         if(message.content.find('<:o7:308408906344824852>') != -1 or
            message.content.find('o7') != -1
            ):
-            yield from client.add_reaction(message,
-                                           ':o7:308408906344824852')
+            yield from client.add_reaction(
+                message,
+                ':o7:308408906344824852')
         # wew
         if re.search(r'(w\s?e\s?w(\slad)?)+\b', x):
-            yield from client.add_reaction(message,
-                                           ':wew:319973823040716804')
-            yield from client.send_message(chan,
-                                           'wew')
+            yield from client.add_reaction(
+                message,
+                ':wew:319973823040716804')
+            yield from client.send_message(
+                chan,
+                'wew')
 
         # v i s i o n
         if re.search(r'(v\s?i\s?s\s?i\s?o\s?n)+', x):
@@ -161,49 +200,33 @@ def on_message(message):
 
     # react to being mentioned
     if message.content.find('<@!319826689729232897>') != -1:
-        yield from client.add_reaction(message,
-                                       ':anime_smug:319973746825756683')
-        yield from client.send_message(chan,
-                                       'You noticed me, senpai.')
+        yield from client.add_reaction(
+            message,
+            ':anime_smug:319973746825756683')
+        yield from client.send_message(
+            chan,
+            'You noticed me, senpai.')
 
     # sira-bot is patriotic
     if message.content.find('*bombs u*') != -1:
-        yield from client.add_reaction(message,
-                                       u"\U0001F4A3")
+        yield from client.add_reaction(
+            message,
+            u"\U0001F4A3")
         yield from client.send_message(
             chan,
             'Space Ireland will be free! <:space_ireland:309204831548211201>')
 
-    # flag
-    if message.content.startswith('!flag'):
-        yield from client.send_file(
-            chan,
-            "C:/SIRA/SIRA-Bot-Rebirth/flag_of_space_ireland.png")
-
-    # space legs
-    if(re.search(r'(s\s?p\s?a\s?c\s?e\s*l\s?e\s?g\s?s)+\b', x) or
-       re.search(r'(a\s?t\s?m\s?o\s?s\s?p\s?h\s?e\s?r\s?i\s?c)+', x)
+    # soon
+    if(re.search(
+            r'(s\s?p\s?a\s?c\s?e\s*l\s?e\s?g\s?s)+\b',
+            x) or
+       re.search(
+            r'(a\s?t\s?m\s?o\s?s\s?p\s?h\s?e\s?r\s?i\s?c)+',
+            x)
        ):
         yield from client.send_message(
             chan,
             'SOON:tm: <:smiling_man:332954734975647754>')
-
-    # server status
-    if message.content.startswith('!server'):
-        sstatus, smsg = yield from check_server()
-        if sstatus == 'success':
-            yield from client.send_message(
-                chan,
-                'FDev says "%s". :ok_hand:' % smsg)
-        elif sstatus == 'warning':
-            yield from client.send_message(
-                chan,
-                ':warning: FDev says "%s".' % smsg)
-        elif sstatus == 'danger':
-            yield from client.send_message(
-                chan,
-                ':fire: "%s". Sandro tripped over the server cords again.' 
-                % smsg)
 
     # if debug is enabled print a message log in the console
     if debug:
