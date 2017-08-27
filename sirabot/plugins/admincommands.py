b6229ca19f5b3d9cf972feca634137074b5da766
N2O-Powered <mail@adobro.online>
2017-08-26 23:47:41 -0400

    CAPS LOCK IS CRUISE CONTROL FOR COOL!

--- sirabot/plugins/admincommands.py
+++ sirabot/plugins/admincommands.py
@@ -20,7 +20,7 @@ async def idle(client, message, parameter):
 # vision status
 @is_admin
 async def vision(client, message, parameter):
-    await client.change_presence(game=discord.Game(name='the v i s i o n'),
+    await client.change_presence(game=discord.Game(name='the V I S I O N'),
                                  status=discord.Status('online'),
                                  afk=False)
     await client.send_message(message.channel, 'I have been visioned.')
